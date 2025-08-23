d38f78c21bcb0a5807dd738023de52b790fa8b31
Philippe Ducasse <ducassephi@hotmail.fr>
2025-08-23 10:48:33 +0200

    added refresh festival hook

---
+++ src/components/page-components/festival-page/helpers/refreshFestival.ts
@@ -0,0 +1,14 @@
+import { AppDispatch } from "@/redux/store";
+import festivalApiService from "@/api/festivalApiService";
+import { setFestival } from "@/redux/slices/festivalSlice";
+
+export const refreshFestival = async (festivalId: number, dispatch: AppDispatch) => {
+  try {
+    const newFestival = await festivalApiService.getFestival(festivalId);
+    if (newFestival) {
+      dispatch(setFestival(newFestival));
+    }
+  } catch (error) {
+    console.error("Failed to fetch festival:", error);
+  }
+};
