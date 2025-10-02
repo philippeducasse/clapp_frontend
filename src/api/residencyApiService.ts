a03d4cac52dbb882699c81f0fa13a17fc814435e
philippeducasse <ducassephi@hotmail.fr>
2025-10-02 21:30:06 +0200

    fixed residency api

--- src/api/residencyApiService.ts
+++ src/api/residencyApiService.ts
@@ -1,7 +1,7 @@
 import { Residency } from "@/interfaces/entities/Residency";
 import { fetchRequest, sendRequest, deleteRequest } from "./fetchHelper";
 
-const endpoint = "/api/residencies/";
+const endpoint = "http://localhost:8000/api/residencies/";
 
 const getResidencies = async (): Promise<Residency[]> => {
   return await fetchRequest(endpoint);
