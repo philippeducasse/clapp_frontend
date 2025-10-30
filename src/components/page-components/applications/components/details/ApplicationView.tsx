f32ef7b6e54cfe9276e12b138067218f6834fc20
philippeducasse <ducassephi@hotmail.fr>
2025-10-30 21:31:17 +0100

    added refresh application

--- src/components/page-components/applications/components/details/ApplicationView.tsx
+++ src/components/page-components/applications/components/details/ApplicationView.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect } from "react";
 import { Clipboard } from "lucide-react";
 import EditButton from "@/components/common/buttons/EditButton";
 import { useSelector } from "react-redux";
@@ -11,13 +12,21 @@ import { Skeleton } from "@/components/ui/skeleton";
 import DetailsViewHeader from "@/components/common/details-view/DetailsViewHeader";
 import DetailsViewSection from "@/components/common/details-view/DetailsViewSection";
 import DetailsViewWrapper from "@/components/common/details-view/DetailsViewWrapper";
+import { refreshApplication } from "../../helpers/refreshApplication";
+import { useDispatch } from "react-redux";
 
 const ApplicationView = () => {
   const params = useParams();
   const applicationId = Number(params.id);
-  const application = useSelector((state: RootState) =>
-    selectApplication(state, applicationId)
-  );
+  const dispatch = useDispatch();
+
+  const application = useSelector((state: RootState) => selectApplication(state, applicationId));
+
+  useEffect(() => {
+    if (!application) {
+      refreshApplication(applicationId, dispatch);
+    }
+  }, [applicationId, application, dispatch]);
 
   if (!application) {
     return <Skeleton />;
@@ -27,15 +36,8 @@ const ApplicationView = () => {
     <DetailsViewWrapper href="/applications">
       <DetailsViewHeader
         title={application.emailSubject ?? ""}
-        icon={
-          <Clipboard
-            className="text-emerald-600 dark:text-emerald-400"
-            size={32}
-          />
-        }
-        actionElements={
-          <EditButton href={`/applications/${application.id}/edit`} />
-        }
+        icon={<Clipboard className="text-emerald-600 dark:text-emerald-400" size={32} />}
+        actionElements={<EditButton href={`/applications/${application.id}/edit`} />}
       />
       <DetailsViewSection
         title="Application information"
