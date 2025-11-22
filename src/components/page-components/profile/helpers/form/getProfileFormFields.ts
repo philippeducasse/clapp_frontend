ccd63e4190780a1af379dd88e7bb0b5ea9b817d5
philippeducasse <ducassephi@hotmail.fr>
2025-11-22 12:35:14 +0100

    added editprofile page

---
+++ src/components/page-components/profile/helpers/form/getProfileFormFields.ts
@@ -0,0 +1,89 @@
+import { ControlledFormElement } from "@/interfaces/forms/ControlledFormElement";
+import { ControlledFormElementType } from "@/interfaces/forms/ControlledFormElementType";
+
+export const getProfileFormFields = (): ControlledFormElement[] => {
+  const fields: ControlledFormElement[] = [
+    {
+      label: "Profile ID",
+      fieldName: "id",
+      type: ControlledFormElementType.NUMBER,
+      hidden: true,
+    },
+    // Basic Information
+    {
+      label: "Email",
+      fieldName: "email",
+      type: ControlledFormElementType.EMAIL,
+      required: true,
+    },
+    {
+      label: "First Name",
+      fieldName: "firstName",
+      type: ControlledFormElementType.TEXT,
+    },
+    {
+      label: "Last Name",
+      fieldName: "lastName",
+      type: ControlledFormElementType.TEXT,
+    },
+    {
+      label: "Artist Name",
+      fieldName: "artistName",
+      type: ControlledFormElementType.TEXT,
+      helpText: "Stage or artist name",
+    },
+    {
+      label: "Company Name",
+      fieldName: "companyName",
+      type: ControlledFormElementType.TEXT,
+      helpText: "Company or organization name",
+    },
+    {
+      label: "Location",
+      fieldName: "location",
+      type: ControlledFormElementType.TEXT,
+    },
+    {
+      label: "Nationality",
+      fieldName: "nationality",
+      type: ControlledFormElementType.TEXT,
+    },
+    {
+      label: "Age",
+      fieldName: "age",
+      type: ControlledFormElementType.NUMBER,
+    },
+    // Contact Information
+    {
+      label: "Personal Website",
+      fieldName: "personalWebsite",
+      type: ControlledFormElementType.URL,
+    },
+    {
+      label: "Instagram Profile",
+      fieldName: "instagramProfile",
+      type: ControlledFormElementType.URL,
+      helpText: "Full URL to Instagram profile",
+    },
+    {
+      label: "Facebook Profile",
+      fieldName: "facebookProfile",
+      type: ControlledFormElementType.URL,
+      helpText: "Full URL to Facebook profile",
+    },
+    {
+      label: "YouTube Profile",
+      fieldName: "youtubeProfile",
+      type: ControlledFormElementType.URL,
+      helpText: "Full URL to YouTube channel",
+    },
+    {
+      label: "TikTok Profile",
+      fieldName: "tiktokProfile",
+      type: ControlledFormElementType.URL,
+      helpText: "Full URL to TikTok profile",
+    },
+  ];
+
+  return fields;
+};
