747d27108e7a5d8134050c9553ac425ac201ffc1
philippeducasse <ducassephi@hotmail.fr>
2025-11-25 19:09:31 +0100

    created login form wip

--- src/components/page-components/auth/components/LoginForm.tsx
+++ src/components/page-components/auth/components/LoginForm.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+"use client";
+
+import { useState } from "react";
+import { createZodFormSchema } from "@/helpers/formHelper";
+import { profileApiService } from "@/api/profileApiService";
+import BasicForm from "@/components/common/form/BasicForm";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { getAuthFormFields } from "../helpers/getAuthFormFields";
+import { useRouter } from "next/navigation";
 
 const LoginForm = () => {
-  return <div>LoginForm</div>;
+  const router = useRouter();
+
+  const formFields = getAuthFormFields();
+  const formSchema = createZodFormSchema(formFields);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    mode: "onSubmit",
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    setIsLoading(true);
+    try {
+      await profileApiService.createProfile(values);
+      router.push(`/login`);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <BasicForm
+      form={form}
+      formFields={formFields}
+      onSubmit={onSubmit}
+      onCancelHref={"index"}
+      isLoading={isLoading}
+    />
+  );
 };
 
 export default LoginForm;
