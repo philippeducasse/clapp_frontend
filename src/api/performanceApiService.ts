2cd975b2f5ab78df5126ca3ba8270ef127c3ca81
ducasse@vdpresearch.de <ducasse@vdpresearch.de>
2025-12-02 15:32:59 +0100

    fixed performance form

--- src/api/performanceApiService.ts
+++ src/api/performanceApiService.ts
@@ -3,15 +3,15 @@ import { fetchRequest, sendRequest, deleteRequest } from "./fetchHelper";
 
 const endpoint = "http://localhost:8000/api/performances/";
 
-const getPerformances = (userId: number): Promise<Performance[]> => {
+const getAll = (userId: number): Promise<Performance[]> => {
   return fetchRequest<Performance[]>(`${endpoint}${userId}/`);
 };
 
-const getPerformance = (performanceId: number): Promise<Performance> => {
+const get = (performanceId: number): Promise<Performance> => {
   return fetchRequest<Performance>(`${endpoint}${performanceId}/`);
 };
 
-const createPerformance = (performance: Performance): Promise<Performance> => {
+const create = (performance: Performance): Promise<Performance> => {
   return sendRequest<Performance, Performance>(
     `${endpoint}`,
     performance,
@@ -20,14 +20,11 @@ const createPerformance = (performance: Performance): Promise<Performance> => {
   );
 };
 
-const deletePerformance = (performanceId: number) => {
-  return deleteRequest(
-    `${endpoint}${performanceId}`,
-    "Performance successfully deleted"
-  );
+const remove = (performanceId: number) => {
+  return deleteRequest(`${endpoint}${performanceId}`, "Performance successfully deleted");
 };
 
-const updatePerformance = (performance: Performance): Promise<Performance> => {
+const update = (performance: Performance): Promise<Performance> => {
   return sendRequest<Performance, Performance>(
     `${endpoint}${performance.id}/`,
     performance,
@@ -37,9 +34,9 @@ const updatePerformance = (performance: Performance): Promise<Performance> => {
 };
 
 export const performanceApiService = {
-  getPerformances,
-  getPerformance,
-  createPerformance,
-  updatePerformance,
-  deletePerformance,
+  getAll,
+  get,
+  create,
+  update,
+  remove,
 };
