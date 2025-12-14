d8ffc1ff96da593b7bf77dafb269efd1b4dad4c7
philippeducasse <ducassephi@hotmail.fr>
2025-12-14 21:33:38 +0100

    added tabs

---
+++ src/components/common/details-view/DetailsTabs.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Tabs, TabsContent } from "@/components/ui/tabs";
+import TabListing from "./TabList";
+
+interface TabProps {
+  name: string;
+  children: React.ReactNode;
+}
+
+export const Tab = ({ children }: TabProps) => {
+  return <>{children}</>;
+};
+
+interface DetailsTabsProps {
+  defaultTab: string;
+  children: React.ReactElement<TabProps> | React.ReactElement<TabProps>[];
+}
+
+const DetailsTabs = ({ defaultTab, children }: DetailsTabsProps) => {
+  const tabs = React.Children.toArray(children) as React.ReactElement<TabProps>[];
+
+  const tabSections = tabs.map((tab) => ({
+    key: tab.props.name.toLowerCase().replace(/\s+/g, "-"),
+    name: tab.props.name,
+  }));
+
+  return (
+    <Tabs defaultValue={defaultTab} className="w-full">
+      <TabListing viewSections={tabSections} />
+      {tabs.map((tab) => {
+        const key = tab.props.name.toLowerCase().replace(/\s+/g, "-");
+        return (
+          <TabsContent key={key} value={key}>
+            {tab.props.children}
+          </TabsContent>
+        );
+      })}
+    </Tabs>
+  );
+};
+
+export default DetailsTabs;
