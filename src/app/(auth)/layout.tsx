a1104809725f4d50980580060bbea1b7a12ecc00
ducasse@vdpresearch.de <ducasse@vdpresearch.de>
2026-01-06 21:05:14 +0100

    fixed auth hydration error

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,11 @@
+import StoreProvider from "@/redux/StoreProvider";
+
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen mt-24 justify-center bg-background">
-      <div className="w-full max-w-lg p-8">{children}</div>
-    </div>
+    <StoreProvider>
+      <div className="flex min-h-screen mt-24 justify-center bg-background">
+        <div className="w-full max-w-lg p-8">{children}</div>
+      </div>
+    </StoreProvider>
   );
 }
