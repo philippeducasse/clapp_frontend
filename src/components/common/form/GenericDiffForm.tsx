e781cecf3650811c61b0965e5b66e3fbc44ffcef
ducasse@vdpresearch.de <ducasse@vdpresearch.de>
2026-01-07 15:47:40 +0100

    refactored update compoenents

---
+++ src/components/common/form/GenericDiffForm.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import React, { Dispatch, SetStateAction, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { isEqual } from "lodash";
+import {
+  createZodFormSchema,
+  sanitizeFormData,
+  getControlledInputs,
+} from "@/helpers/formHelper";
+import { ControlledFormElement } from "@/interfaces/forms/ControlledFormElement";
+import {
+  Form,
+  FormField,
+  FormItem,
+  FormMessage,
+  FormControl,
+} from "@/components/ui/form";
+import { TableCell, TableRow } from "@/components/ui/table";
+
+export interface GenericDiffFormProps<T> {
+  updated: T;
+  changedFields?: (keyof T)[];
+  setUpdated: Dispatch<SetStateAction<T | undefined>>;
+  formFields: ControlledFormElement[];
+}
+
+const GenericDiffForm = <T extends Record<string, unknown>>({
+  updated,
+  changedFields = [],
+  setUpdated,
+  formFields,
+}: GenericDiffFormProps<T>) => {
+  const formSchema = createZodFormSchema(formFields);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: sanitizeFormData(updated) as z.infer<typeof formSchema>,
+  });
+
+  const watchedValues = form.watch();
+
+  useEffect(() => {
+    const updatedEntity = { ...updated, ...watchedValues };
+    if (!isEqual(updatedEntity, updated)) {
+      setUpdated(updatedEntity);
+    }
+  }, [watchedValues, updated, setUpdated]);
+
+  return (
+    <Form {...form}>
+      {formFields
+        .filter((ff) => !ff.hidden)
+        .map((formField) => {
+          const isChanged = changedFields.includes(
+            formField.fieldName as keyof typeof updated
+          );
+
+          return (
+            <TableRow
+              key={formField.fieldName}
+              className={
+                isChanged ? "bg-emerald-50 dark:bg-emerald-950" : undefined
+              }
+            >
+              <TableCell className="align-top w-full">
+                <FormField
+                  control={form.control}
+                  name={formField.fieldName as string}
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormControl>
+                        {getControlledInputs(formField, field, false)}
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </TableCell>
+            </TableRow>
+          );
+        })}
+    </Form>
+  );
+};
+
+export default GenericDiffForm;
--- src/components/page-components/residencies/components/update/ResidencyDiffTable.tsx
+++ src/components/page-components/residencies/components/update/ResidencyDiffTable.tsx
@@ -1,15 +1,8 @@
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import { Residency } from "@/interfaces/entities/Residency";
 import ResidencyDiffForm from "../form/ResidencyDiffForm";
 import { getResidencyFormFields } from "../../helpers/form/getResidencyFormFields";
 import { Dispatch, SetStateAction } from "react";
+import { GenericDiffTable } from "@/components/common/table/GenericDiffTable";
 
 export interface DiffViewProps {
   original: Residency;
@@ -23,65 +16,14 @@ export const ResidencyDiffTable = ({
   setUpdated,
 }: DiffViewProps) => {
   const formFields = getResidencyFormFields();
-  const changedFields = formFields
-    .map((f) => f.fieldName)
-    .filter(
-      (field) =>
-        original[field as keyof Residency] !== updated[field as keyof Residency]
-    );
 
   return (
-    <div className="flex">
-      <div className="w-1/2">
-        <Table className="table-fixed">
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-1/3">Field</TableHead>
-              <TableHead className="w-2/3">Original</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {formFields
-              .filter((f) => !f.hidden)
-              .map((field) => {
-                const fieldName = field.fieldName as keyof Residency;
-                const originalVal = original[fieldName] ?? "";
-                const hasChanged = changedFields.includes(fieldName as string);
-
-                return (
-                  <TableRow key={fieldName}>
-                    <TableCell className="font-medium truncate">
-                      {field.label}
-                    </TableCell>
-                    <TableCell
-                      className={`truncate ${
-                        hasChanged ? "bg-red-50 dark:bg-red-950" : ""
-                      }`}
-                    >
-                      {String(originalVal)}
-                    </TableCell>
-                  </TableRow>
-                );
-              })}
-          </TableBody>
-        </Table>
-      </div>
-      <div className="w-1/2">
-        <Table className="table-fixed">
-          <TableHeader>
-            <TableRow>
-              <TableHead className="w-full">Updated</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <ResidencyDiffForm
-              updatedResidency={updated}
-              changedFields={changedFields}
-              setUpdated={setUpdated}
-            />
-          </TableBody>
-        </Table>
-      </div>
-    </div>
+    <GenericDiffTable
+      original={original}
+      updated={updated}
+      setUpdated={setUpdated}
+      formFields={formFields}
+      DiffFormComponent={ResidencyDiffForm}
+    />
   );
 };
