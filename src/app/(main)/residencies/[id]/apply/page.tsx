94158dcb817f698278728cad1d6597141dd152f4
philippeducasse <ducassephi@hotmail.fr>
2026-01-09 12:54:04 +0100

    added residency application page

---
+++ src/app/(main)/residencies/[id]/apply/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ApplicationForm from "@/components/page-components/applications/components/form/ApplicationForm";
+
+const ResidencyApplicationPage = () => {
+  return <ApplicationForm />;
+};
+
+export default ResidencyApplicationPage;
