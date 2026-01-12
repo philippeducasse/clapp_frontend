ab50b544bb85b2b48b1b048e7174672568997f79
philippeducasse <ducassephi@hotmail.fr>
2026-01-12 11:28:22 +0100

    styled toast

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -1,25 +1,30 @@
-"use client"
+"use client";
 
-import { useTheme } from "next-themes"
-import { Toaster as Sonner, ToasterProps } from "sonner"
+import { CheckCircle2, XCircle } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Toaster as Sonner, ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme = "system" } = useTheme();
 
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
       className="toaster group"
+      icons={{
+        success: <CheckCircle2 className="size-5 text-emerald-600" />,
+        error: <XCircle className="size-5 text-red-500" />,
+      }}
       style={
         {
           "--normal-bg": "var(--popover)",
           "--normal-text": "var(--popover-foreground)",
-          "--normal-border": "var(--border)",
+          // "--normal-border": "var(--border)",
         } as React.CSSProperties
       }
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
