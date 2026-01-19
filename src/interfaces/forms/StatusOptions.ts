0dee5823822cffce448b809d38286d8bc53cfb48
philippeducasse <ducassephi@hotmail.fr>
2026-01-19 17:09:54 +0100

    standardised enum formatting

--- src/interfaces/forms/StatusOptions.ts
+++ src/interfaces/forms/StatusOptions.ts
@@ -2,14 +2,13 @@ import { SelectOptions } from "./ControlledFormElement";
 import { ApplicationStatus } from "../entities/Application";
 
 export const ApplicationStatusOptions: SelectOptions[] = [
-  { value: ApplicationStatus.ACCEPTED, label: "Accepted" },
-  { value: ApplicationStatus.APPLIED, label: "Applied" },
-  { value: ApplicationStatus.CANCELLED, label: "Cancelled" },
-  { value: ApplicationStatus.IGNORED, label: "Ignored" },
-  { value: ApplicationStatus.IN_DISCUSSION, label: "In discussion" },
-  { value: ApplicationStatus.NOT_APPLIED, label: "Not Applied" },
-  { value: ApplicationStatus.OTHER, label: "Other" },
-  { value: ApplicationStatus.POSTPONED, label: "Postponed" },
-  { value: ApplicationStatus.REJECTED, label: "Rejected" },
   { value: ApplicationStatus.DRAFT, label: "Draft" },
+  { value: ApplicationStatus.APPLIED, label: "Applied" },
+  { value: ApplicationStatus.IN_DISCUSSION, label: "In discussion" },
+  { value: ApplicationStatus.REJECTED, label: "Rejected" },
+  { value: ApplicationStatus.IGNORED, label: "Ignored" },
+  { value: ApplicationStatus.ACCEPTED, label: "Accepted" },
+  { value: ApplicationStatus.POSTPONED, label: "Postponed" },
+  { value: ApplicationStatus.CANCELLED, label: "Cancelled" },
+  { value: ApplicationStatus.OTHER, label: "Other" },
 ];
