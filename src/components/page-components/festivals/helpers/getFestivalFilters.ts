83aa393a7a55dacec7c43fa2faba021a8fec1291
philippeducasse <ducassephi@hotmail.fr>
2026-01-19 17:09:15 +0100

    removed useless status

--- src/components/page-components/festivals/helpers/getFestivalFilters.ts
+++ src/components/page-components/festivals/helpers/getFestivalFilters.ts
@@ -44,7 +44,6 @@ export const getFestivalFilters = (): FilterConfig[] => {
       type: FilterType.SELECT,
       options: [
         { value: "APPLIED", label: "Applied" },
-        { value: "NOT_APPLIED", label: "Not applied" },
       ],
     },
   ];
