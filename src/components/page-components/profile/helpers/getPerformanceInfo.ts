6f59c65fa2a91c30abe7e9b8fe4930b8e2573295
philippeducasse <ducassephi@hotmail.fr>
2026-01-24 13:21:20 +0100

    added missing fields to details views

--- src/components/page-components/profile/helpers/getPerformanceInfo.ts
+++ src/components/page-components/profile/helpers/getPerformanceInfo.ts
@@ -17,7 +17,8 @@ export const getPerformanceInfo = (performance: Performance): SectionCellProps[]
     { title: "Short Description", value: performance.shortDescription, type: SectionCellType.HTML },
     { title: "Long Description", value: performance.longDescription, type: SectionCellType.HTML },
     { title: "Email prompt", value: performance.emailPrompt, type: SectionCellType.HTML },
-    { title: "Creation Date", value: performance.trailer, type: SectionCellType.Link },
+    { title: "Creation Date", value: performance.creationDate },
+    { title: "Trailer", value: performance.trailer, type: SectionCellType.Link },
     {
       title: "Dossiers",
       value: performance.dossiers
