2d98af5405dbed95535838a6593dd653ff04877e
philippeducasse <ducassephi@hotmail.fr>
2026-02-20 11:22:56 +0100

    fix(middleware): added exclusion for confirm-email page

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,19 @@ import { NextResponse, NextRequest } from "next/server";
 export const middleware = (request: NextRequest) => {
   const sessionid = request.cookies.get("sessionid");
   const { pathname } = request.nextUrl;
-  if (sessionid && (pathname === "/" || pathname.startsWith("/login") || pathname.startsWith("/register"))) {
+  if (
+    sessionid &&
+    (pathname === "/" || pathname.startsWith("/login") || pathname.startsWith("/register"))
+  ) {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
-  if (!sessionid && pathname !== "/" && !pathname.startsWith("/login") && !pathname.startsWith("/register")) {
+  if (
+    !sessionid &&
+    pathname !== "/" &&
+    !pathname.startsWith("/login") &&
+    !pathname.startsWith("/register") &&
+    !pathname.startsWith("/email-confirmation")
+  ) {
     return NextResponse.redirect(new URL("/", request.url));
   }
   return NextResponse.next();
